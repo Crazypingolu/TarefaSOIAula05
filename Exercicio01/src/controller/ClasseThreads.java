91fb3e4e54696dfb264a2a5e41aa0567043d02b6
Lucas <lucaswar05@gmail.com>
2024-09-11 01:57:32 -0300

    Exe 01 - Organization and all the code comments

--- Exercicio01/src/controller/ClasseThreads.java
+++ Exercicio01/src/controller/ClasseThreads.java
@@ -2,34 +2,34 @@ package controller;
 import java.util.concurrent.Semaphore;
 import java.util.Random;
 public class ClasseThreads extends Thread {
-    // variáveis:
+    // Definir variáveis (privadas da classe):
     private int idBancarrio;
     private Semaphore semaforo;
-    
+    // Gerar construtores:
     public ClasseThreads(int idBancarrio, Semaphore semaforo){
         this.idBancarrio = idBancarrio;
         this.semaforo = semaforo;
     }
-
+    // Método para simular o calculo (baseado no index)
     public void calc(int resto, int idBancarrio, int indx) {
         Random rad = new Random();
         int t;
         if (resto == 1){
-            t = rad.nextInt(800) + 200;
+            t = rad.nextInt(800) + 200; // min = 0.2, max = 1s
         } else if (resto == 2) {
-            t = rad.nextInt(1000) + 500;
+            t = rad.nextInt(1000) + 500; // min = 0.5s, max = 1.5s
         } else { // resto == 0
-            t = rad.nextInt(1000) + 1000;
+            t = rad.nextInt(1000) + 1000; // min = 1s, max = 2s
         }
         try {
             sleep(t);
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            System.out.println("#" + idBancarrio + ": Cálculo " + (indx + 1) + "concluído!");
+            System.out.println("#" + idBancarrio + ":" + (indx + 1) + "º cálculo concluído!");
         }
     }
-
+    // Método para simular a transação com BD
     public void tran(int resto, int idBancarrio, int indx) {
         int t;
         if (resto == 1){
@@ -42,28 +42,28 @@ public class ClasseThreads extends Thread {
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            System.out.println("#" + idBancarrio + ": Transação " + (indx + 1) + " concluída!");
+            System.out.println("#" + idBancarrio + ":" + (indx + 1) + "º transação concluída!");
         }
     }
 
     @Override
     public void run(){
-        int resto = idBancarrio % 3;
+        int resto = idBancarrio % 3; // Gera o fator de diferença para o processamento
         int loop = 0;
         if (resto == 1){
             loop = 2;
-        } else {
+        } else { // resto == 0 && resto == 2
             loop = 3;
         }
         for (int i = 0; i < loop; i++) {
-            calc(resto, idBancarrio, i);
+            calc(resto, idBancarrio, i); // Chama a simulação de cálculo:
             try {
-                semaforo.acquire();
-                tran(resto, idBancarrio, i);
+                semaforo.acquire(); // Chama o semáforo
+                tran(resto, idBancarrio, i); // Executa a simulação com BD
             } catch (Exception e) {
                 e.printStackTrace();
             } finally {
-                semaforo.release();
+                semaforo.release(); // Libera o semáforo
             }
         }
     }
--- Exercicio01/src/view/ClassePrincipal.java
+++ Exercicio01/src/view/ClassePrincipal.java
@@ -1,12 +1,13 @@
 package view;
+// Trazer Bibliotecas e Pacotes
 import java.util.concurrent.Semaphore;
 import controller.ClasseThreads;
-
 public class ClassePrincipal {
     public static void main(String[] args) {
-        int perm = 1;
-        Semaphore semaforo = new Semaphore(perm);
-        
+        // Definir variáveis:
+        int perm = 1; // quantas Transações podem ser feitas simultaneamente
+        Semaphore semaforo = new Semaphore(perm); // Criação do semáforo
+        // processamento + saída de dados:
         for (int i = 0; i < 22; i++) {
             ClasseThreads t = new ClasseThreads(i, semaforo);
             t.start();
