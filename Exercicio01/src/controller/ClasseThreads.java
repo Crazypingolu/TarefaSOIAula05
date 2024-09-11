da3d11505180dfdb86514df446aa9229058f5dab
Lucas <lucaswar05@gmail.com>
2024-09-11 01:46:39 -0300

    Exe 01 - All methods created and running first test

--- Exercicio01/src/controller/ClasseThreads.java
+++ Exercicio01/src/controller/ClasseThreads.java
@@ -1,39 +1,70 @@
 package controller;
 import java.util.concurrent.Semaphore;
+import java.util.Random;
 public class ClasseThreads extends Thread {
     // variáveis:
     private int idBancarrio;
-    private int resto;
     private Semaphore semaforo;
-    // instância:
-    ClasseMetodos met = new ClasseMetodos();
     
     public ClasseThreads(int idBancarrio, Semaphore semaforo){
         this.idBancarrio = idBancarrio;
         this.semaforo = semaforo;
     }
 
-    @Override
-    public void run(){
-        resto = met.calculo(idBancarrio);
-        switch (resto) {
-            case 1: // resto == 1
-                
-                break;
-            case 2: // resto == 2
-
-                break;
-            case 3: // resto == 0
-
-                break;
+    public void calc(int resto, int idBancarrio, int indx) {
+        Random rad = new Random();
+        int t;
+        if (resto == 1){
+            t = rad.nextInt(800) + 200;
+        } else if (resto == 2) {
+            t = rad.nextInt(1000) + 500;
+        } else { // resto == 0
+            t = rad.nextInt(1000) + 1000;
         }
-        try { 
-            semaforo.acquire();
-        } catch (Exception e){
-            
+        try {
+            sleep(t);
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
-            semaforo.release();
+            System.out.println("#" + idBancarrio + ": Cálculo " + (indx + 1) + "concluído!");
         }
     }
 
+    public void tran(int resto, int idBancarrio, int indx) {
+        int t;
+        if (resto == 1){
+            t = 1000;
+        } else { // resto == 0 & resto == 2
+            t = 1500;
+        }
+        try {
+            sleep(t);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("#" + idBancarrio + ": Transação " + (indx + 1) + " concluída!");
+        }
+    }
+
+    @Override
+    public void run(){
+        int resto = idBancarrio % 3;
+        int loop = 0;
+        if (resto == 1){
+            loop = 2;
+        } else {
+            loop = 3;
+        }
+        for (int i = 0; i < loop; i++) {
+            calc(resto, idBancarrio, i);
+            try {
+                semaforo.acquire();
+                tran(resto, idBancarrio, i);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                semaforo.release();
+            }
+        }
+    }
 }
--- Exercicio01/src/view/ClassePrincipal.java
+++ Exercicio01/src/view/ClassePrincipal.java
@@ -1,7 +1,15 @@
 package view;
+import java.util.concurrent.Semaphore;
+import controller.ClasseThreads;
 
 public class ClassePrincipal {
     public static void main(String[] args) {
+        int perm = 1;
+        Semaphore semaforo = new Semaphore(perm);
         
+        for (int i = 0; i < 22; i++) {
+            ClasseThreads t = new ClasseThreads(i, semaforo);
+            t.start();
+        }
     }
 }
